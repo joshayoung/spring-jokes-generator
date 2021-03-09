a57286b1ad60f1b8bd0beac2105fd04ba9a2df64
Josh Young <18503267+joshayoung@users.noreply.github.com>
2021-03-09 13:15:49 -0600

    Create project with initializr

---
+++ src/main/java/com/joshayoung/springjokesgenerator/SpringJokesGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.joshayoung.springjokesgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJokesGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJokesGeneratorApplication.class, args);
+    }
+
+}
