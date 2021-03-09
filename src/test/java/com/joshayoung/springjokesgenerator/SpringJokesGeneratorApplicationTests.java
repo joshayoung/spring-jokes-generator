e9f639b302a2739f8e03b8da6a6450a34e582507
Josh Young <j22cal@gmail.com>
2021-03-09 13:15:49 -0600

    Create project with initializr

---
+++ src/test/java/com/joshayoung/springjokesgenerator/SpringJokesGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joshayoung.springjokesgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJokesGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
